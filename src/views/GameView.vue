2e538cc35278865ca10bc631114442ec132a0afb
ridvansumset <ridvansumset@gmail.com>
2023-03-13 20:04:37 +0300

    add dummy stream info

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -4,7 +4,29 @@
       <div
         class="video-stream"
         :class="chatVisible ? '' : 'video-stream-full-width'"
-      />
+      >
+        <h2>VIDEO STREAM</h2>
+      </div>
+
+      <div class="data-container">
+        <div class="stream-info">
+          <div class="streamer-pp-container">
+            <img src="@/assets/logo.png" alt="streamer profile photo" width="40" height="40" />
+          </div>
+
+          <div class="stream-detail-info">
+            <p class="streamer-name">
+              tenekekafalar
+            </p>
+
+            <p class="stream-title">
+              Rocket League'de Elo Kasmaca !drop !settings
+            </p>
+          </div>
+        </div>
+
+        <div class="donation-container"></div>
+      </div>
     </main>
 
     <game-chat
@@ -42,11 +64,40 @@ export default {
   width: calc(100% - $chatWidth);
   height: 600px;
   background-color: gray;
+  display: grid;
+  place-items: center;
   @media all and (max-width: $breakpointSM) {
     width: 70%; // 100% - 30% (chat width is 30% for mobile)
   }
 }
 .video-stream-full-width {
   width: 100% !important;
+}
+.data-container {
+  display: flex;
+  justify-content: space-between;
+  color: $colWhite;
+  padding: 12px 24px;
+}
+.stream-info {
+  display: flex;
+}
+.streamer-pp-container {
+  width: 32px;
+  height: 32px;
+  border: solid 2px $colWhite;
+  border-radius: 25px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  overflow: hidden;
+  margin-right: 8px;
+}
+.streamer-name {
+  margin-bottom: 8px;
+  cursor: pointer;
+}
+.stream-title {
+  font-size: 20px;
 }
 </style>
