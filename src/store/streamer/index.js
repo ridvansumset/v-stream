d345b867ffd5bbbc06dade549e9b5730d2ea4dcd
ridvansumset <ridvansumset@gmail.com>
2023-03-14 20:40:13 +0300

    fix continous donate amount bug

--- src/store/streamer/index.js
+++ src/store/streamer/index.js
@@ -3,14 +3,14 @@ export const streamer = {
   state: () => ({
     username: '',
     title: '',
-    donatedAmount: 1000
+    donatedAmount: 0
   }),
   actions: {
     viewStream ({ commit }, { username, title }) {
-      commit('setStream', { username, title })
+      commit('setStream', { username, title, donatedAmount: Math.floor(Math.random() * 500) })
     },
     clearStream ({ commit }) {
-      commit('setStream', { username: '', title: '' })
+      commit('setStream', { username: '', title: '', donatedAmount: 0 })
     },
     donate ({ state, commit }, { amount }) {
       const total = state.donatedAmount + amount
@@ -21,6 +21,7 @@ export const streamer = {
     setStream (state, value) {
       state.username = value.username
       state.title = value.title
+      state.donatedAmount = value.donatedAmount
     },
     setDonatedAmount (state, value) {
       state.donatedAmount = value
