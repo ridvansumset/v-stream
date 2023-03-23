f7e3f885fc6c1a2569f0de7c81cafc3236da5f58
ridvansumset <ridvansumset@gmail.com>
2023-03-23 22:24:01 +0300

    add subtle animation to the logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="app-header">
-      <div>
+      <div class="app-logo-container">
         <router-link to="/" class="app-logo">V-Stream</router-link>
       </div>
 
@@ -45,12 +45,23 @@ export default {
   box-shadow: -3px 6px 4px -4px #222;
   z-index: 1000;
 }
+.app-logo-container:hover {
+  animation: logo-anim 0.5s linear alternate;
+}
+@keyframes logo-anim {
+  50% {
+    transform: skew(10deg,4deg);
+  }
+  100% {
+    transform: skew(0deg,0deg);
+  }
+}
 .app-logo {
   color: $colBlue;
   margin-left: 24px;
 }
 .app-logo:hover {
-  color: $colDarkBlue;
+  color: $colBlue;
   text-decoration: none;
 }
 .full-name {
